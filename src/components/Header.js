43017cf39ccc970bfd611135ff58a9fd99ea749d
Manish-XD <manish.pgl41@gmail.com>
2022-10-13 17:28:30 +0530

    made society filteration working

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,10 @@ import './Header.css';
 
 const Header = () => {
   return (
-    <header>
       <nav>
-        <img src="/society_logo.png" alt="logo" style={{width:"7%", height:"90%"}}/>
+        <img src="/society_logo.png" alt="logo"/>
         <label class="logo">SOCIETY SPHERE</label>
       </nav>
-    </header>
   )}
 
 export default Header
