be7768f56dc720989a7c767bd3f34b41f48ae7f0
Manish-XD <manish.pgl41@gmail.com>
2022-10-13 16:17:05 +0530

    imporved carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,135 +1,39 @@
 import React from 'react';
 import './Carousel.css';
 
-class Card1 extends React.Component {
-  state = {  } 
-  render() { 
- 
+const Carousel = () => {
     return (
-
-            <section id="slider">
-                <div class="container">
-                    <div class="subcontainer">
-                        <div class="slider-wrapper">
-                            <div class="controller">
-                                <div id="controls">
-                                    <button class="previous"><i class="fa-solid fa-angles-left"></i></button>
-                                    <button class="next"><i class="fa-sharp fa-solid fa-angles-right"></i></button>
-                                </div>
-                                <br/>
-                                <div class="myslider">
-                                    <div>
-                                        <div class="slide">
-                                            <div class="slide-img img-1">
-                                                <a href="/">Learn More</a>
-                                            </div>
-                                            <br/>
-                                            <div>
-                                                <h3>First</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="slide">
-                                            <div class="slide-img img-2">
-                                                <a href="/">Learn More</a>
-                                            </div>
-                                            <br/>
-                                            <div>
-                                                <h3>Second</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="slide">
-                                            <div class="slide-img img-3">
-                                                <a href="/">Learn More</a>
-                                            </div>
-                                            <br/>
-                                            <div>
-                                                <h3>Third</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="slide">
-                                            <div class="slide-img img-4">
-                                                <a href="/">Learn More</a>
-                                            </div>
-                                            <br/>
-                                            <div>
-                                                <h3>Fourth</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="slide">
-                                            <div class="slide-img img-5">
-                                                <a href="/">Learn More</a>
-                                            </div>
-                                            <br/>
-                                            <div>
-                                                <h3>Fifth</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="slide">
-                                            <div class="slide-img img-6">
-                                                <a href="/">Learn More</a>
-                                            </div>
-                                            <br/>
-                                            <div>
-                                                <h3>Six</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="slide">
-                                            <div class="slide-img img-7">
-                                                <a href="/">Learn More</a>
-                                            </div>
-                                            <br/>
-                                            <div>
-                                                <h3>Seventh</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="slide">
-                                            <div class="slide-img img-8">
-                                                <a href="/">Learn More</a>
-                                            </div>
-                                            <br/>
-                                            <div>
-                                                <h3>Eight</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="slide">
-                                            <div class="slide-img img-9">
-                                                <a href="/">Learn More</a>
-                                            </div>
-                                            <br/>
-                                            <div>
-                                                <h3>Nine</h3>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+        <div className='Carousel'>
+        <h1>RECENT EVENTS</h1>
+            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active">
+                        <img src="https://wallpapercave.com/dwp2x/wp3788222.jpg" className="carousel_img" alt="..." />
+                        <img src="https://wallpapercave.com/dwp2x/wp3788222.jpg" className="carousel_img" alt="..." />
+                        <img src="https://wallpapercave.com/dwp2x/wp3788222.jpg" className="carousel_img" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://wallpapercave.com/dwp2x/wp3788222.jpg" className="carousel_img" alt="..." />
+                        <img src="https://wallpapercave.com/dwp2x/wp3788222.jpg" className="carousel_img" alt="..." />
+                        <img src="https://wallpapercave.com/dwp2x/wp3788222.jpg" className="carousel_img" alt="..." />
+                    </div>
+                    <div className="carousel-item">
+                        <img src="https://wallpapercave.com/dwp2x/wp3788222.jpg" className="carousel_img" alt="..." />
+                        <img src="https://wallpapercave.com/dwp2x/wp3788222.jpg" className="carousel_img" alt="..." />
+                        <img src="https://wallpapercave.com/dwp2x/wp3788222.jpg" className="carousel_img" alt="..." />
                     </div>
                 </div>
-        
-        
-        
-            </section>
-      
-    );
-  }
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    )
 }
- 
-export default Card1;
 
+export default Carousel
