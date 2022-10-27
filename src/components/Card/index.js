b194bc73e8fcd258e74ac6f566ee444c51e63f1b
Manish-XD <manish.pgl41@gmail.com>
2022-10-27 10:41:06 +0530

    improved style of flip cards

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import sanityClient from "../../client";
 import ImageUrlBuilder from '@sanity/image-url';
 import { Link } from 'react-router-dom';
+import LinkIcon from '@mui/icons-material/Link';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 const Card = ({ cardData }) => {
 
@@ -26,9 +29,9 @@ const Card = ({ cardData }) => {
                   <p className='para'>{curElem.about.slice(0, 80)}...</p>
                 </Link>
                 <div className='links'>
-                  <a href={curElem.website} target="_blank" rel="noreferrer"><img src="images/Link-icon.jpg" className='img' alt="social" /> </a>
-                  <a href={curElem.instagram} target="_blank" rel="noreferrer"><img src="images/Instagram-icon.jpg" className='img' alt="social" /> </a>
-                  <a href={curElem.linkedin} target="_blank" rel="noreferrer"><img src="images/Linkedin-icon.png" className='img' alt="social" /> </a>
+                  <a href={curElem.website} target="_blank" rel="noreferrer"><LinkIcon/></a>
+                  <a href={curElem.instagram} target="_blank" rel="noreferrer"><InstagramIcon/></a>
+                  <a href={curElem.linkedin} target="_blank" rel="noreferrer"><LinkedInIcon/></a>
                 </div>
               </div>
             </div>
