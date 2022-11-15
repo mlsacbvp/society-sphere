9725a9e3c15f8d4ee13d704b4005c127a67cf1e4
void-ness <78547746+void-ness@users.noreply.github.com>
2022-11-15 16:52:06 +0530

    update: slider

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import './style.css';
+
+import "../../../node_modules/react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 import sanityClient from "../../client";
 import ImageUrlBuilder from '@sanity/image-url';
 
-const Carousel = () => {
+const Slider = () => {
     const [carouselData, setcarouselData] = useState(null);
 
     useEffect(() => {
@@ -13,39 +17,65 @@ const Carousel = () => {
             (res) => { setcarouselData(res) }
         ).catch(console.error);
     }, [])
-
     // console.log(carouselData);
+
+    let carouselDataPosters = []
+    carouselData?.forEach((element) => {
+        carouselDataPosters.push(element.poster1, element.poster2, element.poster3)
+    });
+
+    console.log(carouselDataPosters);
+
     const builder = ImageUrlBuilder(sanityClient);
     function urlFor(source) {
         return builder.image(source);
     }
-
     return (
-        <div id='RecentEventCarousel' className='Carousel'>
-            <h1>RECENT EVENTS</h1>
-            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+        <div id='RecentEventCarousel' className='recent-events-carousel'>
+            <h1 className='carousel-heading'>RECENT EVENTS</h1>
+            <Carousel
+                infiniteLoop={true}
+                autoPlay={true}
+                showIndicators={false}
+                showStatus={false}
+                // showArrows={false}
+                showThumbs={false}
+                interval={5000}
+                className="home-page__web-slider"
+            >
                 {carouselData && carouselData.map((event, index) => {
                     return (
-                        <div className="carousel-inner" key={index}>
-                            <div className={`carousel-item ${index === 0 ? "active" : ""}`}>
-                                <img src={urlFor(event.poster1).url()} className="carousel_img" alt={event.poster1.alt} />
-                                <img src={urlFor(event.poster2).url()} className="carousel_img" alt={event.poster2.alt} />
-                                <img src={urlFor(event.poster3).url()} className="carousel_img" alt={event.poster3.alt} />
-                            </div>
+                        <div className="recent-events-carousel-item">
+                            <img src={urlFor(event.poster1).url()} className="carousel_img" alt={event.poster1.alt} />
+                            <img src={urlFor(event.poster2).url()} className="carousel_img" alt={event.poster2.alt} />
+                            <img src={urlFor(event.poster3).url()} className="carousel_img" alt={event.poster3.alt} />
                         </div>
                     )
                 })}
-                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
-                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Previous</span>
-                </button>
-                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
-                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span className="visually-hidden">Next</span>
-                </button>
-            </div>
+            </Carousel>
+
+            <Carousel
+                infiniteLoop={true}
+                autoPlay={true}
+                // showIndicators={false}
+                showStatus={false}
+                // showArrows={false}
+                showThumbs={false}
+                interval={5000}
+                className="home-page__phone-slider"
+            >
+                {carouselDataPosters && carouselDataPosters.map((event, index) => {
+                    return (
+                        <>
+                            <div className="recent-events-carousel-item">
+                                <img src={urlFor(event).url()} className="carousel_img_phone" alt={event.alt} />
+                            </div>
+                        </>
+                    )
+                })}
+            </Carousel>
         </div>
     )
 }
 
-export default Carousel
+export default Slider;
