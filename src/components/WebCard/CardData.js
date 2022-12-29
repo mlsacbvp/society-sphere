52769f35ee3406aaf02a67a8068922154aa6e9ec
void-ness <78547746+void-ness@users.noreply.github.com>
2022-12-30 01:44:38 +0530

    rename: card -> WebCard

---
+++ src/components/WebCard/CardData.js
@@ -0,0 +1,16 @@
+const Data = [
+  {
+    image: "images/GDSC.png",
+    name: "BVP",
+    category: "Universal",
+    img: "images/Linkedin-icon.png",
+    insta: "images/Instagram-icon.jpg",
+    link2: "https://www.instagram.com/gdscbvp/",
+    site: "images/Link-icon.jpg",
+    link: 'https://www.youtube.com/',
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+  },
+];
+
+export default Data;
